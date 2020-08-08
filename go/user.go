86cdad759366e6b54ffbbda8d9e11a1eaf0a0518
naoto67 <uver.com67@gmail.com>
2020-08-08 11:38:05 +0900

    ユーザ取得N+1を削除

---
+++ go/user.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func FetchUserDictByItems(items []Item) (map[int64]User, error) {
+	var userIDs []string
+	for _, v := range items {
+		if v.BuyerID != 0 {
+			userIDs = append(userIDs, fmt.Sprintf("%d", v.BuyerID))
+		}
+		userIDs = append(userIDs, fmt.Sprintf("%d", v.SellerID))
+	}
+	var users []User
+	err := dbx.Select(&users, "SELECT * FROM users WHERE id IN (?)", strings.Join(userIDs, ","))
+	if err != nil {
+		return nil, err
+	}
+	dict := map[int64]User{}
+	for _, v := range users {
+		dict[v.ID] = v
+	}
+
+	return dict, nil
+}
+
+func FetchUserSimpleDictByItems(items []Item) (map[int64]UserSimple, error) {
+	var userIDs []string
+	for _, v := range items {
+		if v.BuyerID != 0 {
+			userIDs = append(userIDs, fmt.Sprintf("%d", v.BuyerID))
+		}
+		userIDs = append(userIDs, fmt.Sprintf("%d", v.SellerID))
+	}
+	var users []UserSimple
+	err := dbx.Select(&users, "SELECT id, account_name, num_sell_items FROM users WHERE id IN (?)", strings.Join(userIDs, ","))
+	if err != nil {
+		return nil, err
+	}
+	dict := map[int64]UserSimple{}
+	for _, v := range users {
+		dict[v.ID] = v
+	}
+
+	return dict, nil
+}
