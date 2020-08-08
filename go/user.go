b0a032c894167f687586c2b28614c348b3386582
naoto67 <uver.com67@gmail.com>
2020-08-08 11:59:49 +0900

    debug

--- go/user.go
+++ go/user.go
@@ -37,6 +37,7 @@ func FetchUserSimpleDictByItems(items []Item) (map[int64]UserSimple, error) {
 	}
 	var users []UserSimple
 	err := dbx.Select(&users, "SELECT id, account_name, num_sell_items FROM users WHERE id IN (?)", strings.Join(userIDs, ","))
+	fmt.Println("FetchUserSimpleDictByItems: users: ", users)
 	if err != nil {
 		return nil, err
 	}
