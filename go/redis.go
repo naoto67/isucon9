674a811e554f204cadc42822ef5857200c5fa8d5
naoto67 <uver.com67@gmail.com>
2020-08-13 04:58:14 +0900

    修正

--- go/redis.go
+++ go/redis.go
@@ -62,7 +62,7 @@ func (r RedisClient) MGET(key []interface{}) ([][]byte, error) {
 	conn := r.pool.Get()
 	defer conn.Close()
 
-	return redis.ByteSlices(conn.Do("MGET", redis.Args{}.AddFlat(key)...))
+	return redis.ByteSlices(conn.Do("MGET", key...))
 }
 
 func (r RedisClient) HSET(key, field string, value interface{}) error {
