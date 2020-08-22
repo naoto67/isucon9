eb6ee59cdb075697e8cd410d1c93cfa9adf9a6e9
naoto67 <uver.com67@gmail.com>
2020-08-22 14:41:35 +0900

    indexの追加

--- sql/01_schema.sql
+++ sql/01_schema.sql
@@ -30,7 +30,7 @@ CREATE TABLE `items` (
   `category_id` int unsigned NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  INDEX idx_category_id (`category_id`),
+  INDEX idx_category_and_created_at (`category_id`, `created_at`),
   INDEX idx_seller_and_created_at_id (`seller_id`, `created_at`),
   INDEX idx_buyer_id (`buyer_id`),
   INDEX idx_created_at (`created_at`)
