74ee8c4c6f481f7262a6e51763f14afeef37ddae
naoto67 <uver.com67@gmail.com>
2020-09-07 18:57:35 +0900

    sqlの修正

--- go/main.go
+++ go/main.go
@@ -587,12 +587,11 @@ func getUserItems(w http.ResponseWriter, r *http.Request) {
 	if itemID > 0 && createdAt > 0 {
 		// paging
 		err := dbx.Select(&items,
-			"SELECT * FROM `items` WHERE `seller_id` = ? AND `status` IN (?,?,?) AND (`created_at` <= ? AND `id` < ?)) ORDER BY `created_at` DESC, `id` DESC LIMIT ?",
+			"SELECT * FROM `items` WHERE `seller_id` = ? AND `status` IN (?,?,?) AND (`created_at` <= ? AND `id` < ?) ORDER BY `created_at` DESC, `id` DESC LIMIT ?",
 			userSimple.ID,
 			ItemStatusOnSale,
 			ItemStatusTrading,
 			ItemStatusSoldOut,
-			time.Unix(createdAt, 0),
 			time.Unix(createdAt, 0),
 			itemID,
 			ItemsPerPage+1,
